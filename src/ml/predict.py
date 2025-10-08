b10c7f3a18860d3d747d424c307945723f7fb5c0
adalbertobrant <adalbertobrant@gmail.com>
2025-10-08 14:28:59 -0300

    fix spaces

--- src/ml/predict.py
+++ src/ml/predict.py
@@ -3,6 +3,7 @@
 def predict_price(ticker):
     """
     Placeholder function to predict stock price.
-    Can be implemented later with a machine learning model (e.g., Linear Regression).
+    Can be implemented later with a machine learning model
+    (e.g., Linear Regression).
     """
     pass  # To be implemented later
